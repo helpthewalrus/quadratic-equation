a9cf05cc7a44bf4417ca6f6c4cf86aa71564bd79
helpthewalrus <alinatylets@gmail.com>
2018-09-25 00:03:16 +0300

    task_4_commit_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  const equationSplit = equation.split(' ');
+  const a = equationSplit[0];
+  const b = equationSplit[3] + equationSplit[4];
+  const c = equationSplit[7] + equationSplit[8];
+  const d = b**2 - 4*a*c;
+
+  if (d > 0) {
+    const x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    const x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+    const result = [x1, x2];
+    return result.sort((a, b) => a - b); 
+  } else if (d === 0) {
+    const x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    return [x1];
+  } else {
+    return 'There are no roots.'
+  }
+};
